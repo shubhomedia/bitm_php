4f7fd379487556a62007ba603a5e6adae5308566
shubhobd <shubhomedia@gmail.com>
2015-11-23 21:36:08 +0600

    data type file integer add

---
+++ data_types/integer.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+            $decimal_number = 1234;
+            $negetive_number = -1234;
+            $octal_number = 0123; //equivalent to 83 decimal
+            $hexdecimal_number = 0x1A; //equivalent to 26 decimal
+            $binary_number = 0b11111111; //equivalent to 255 decimal
+        ?>
+    </body>
+</html>
