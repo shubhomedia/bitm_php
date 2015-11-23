7a37eaa36c6268f43fc06694f4ca987c913aa4b0
shubhobd <shubhomedia@gmail.com>
2015-11-23 21:39:30 +0600

    integer file code add

--- data_types/integer.php
+++ data_types/integer.php
@@ -16,6 +16,13 @@ and open the template in the editor.
             $octal_number = 0123; //equivalent to 83 decimal
             $hexdecimal_number = 0x1A; //equivalent to 26 decimal
             $binary_number = 0b11111111; //equivalent to 255 decimal
+            
+            echo $decimal_number ."</br>";
+            echo $negetive_number ."</br>";
+            echo $octal_number ."</br>";
+            echo $hexdecimal_number ."</br>";
+            echo $binary_number ."</br>";
+            
         ?>
     </body>
 </html>
