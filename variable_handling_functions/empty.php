6f039a727fcb18717b4d97aa390b17e1c5ef526f
shubhobd <shubhomedia@gmail.com>
2015-11-24 10:54:03 +0600

    empty file add

---
+++ variable_handling_functions/empty.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<!--
+    MD.OWES QURUNY SHUBHO
+    http://shubho.info
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+            $expected_array_got_string = 'somestring';
+            var_dump(empty($expected_array_got_string['some_key']));
+            echo"</br>";
+            var_dump(empty($expected_array_got_string[0]));
+            echo"</br>";
+            var_dump(empty($expected_array_got_string['0']));
+            echo"</br>";
+            var_dump(empty($expected_array_got_string[0.5]));
+            echo"</br>";
+            var_dump(empty($expected_array_got_string['0.5']));
+            echo"</br>";
+            var_dump(empty($expected_array_got_string['0 Mostel']));
+        ?>
+    </body>
+</html>
