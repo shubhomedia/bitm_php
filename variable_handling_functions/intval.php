ea9a0556ef05410e88c8923d42610572e8c498bd
shubhobd <shubhomedia@gmail.com>
2015-11-24 11:11:07 +0600

    intval file created

---
+++ variable_handling_functions/intval.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<!--
+    MD.OWES QURUNY SHUBHO
+    http://shubho.info
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+            //initval — Get the integer value of a variable
+            
+            $number_1 = 100.100;
+            $number_2 = -10.1010;
+            $number_3 = "800a";
+            $number_4 = '100.100';
+                        
+            echo intval($number_1);
+            echo"</br>";
+            echo intval($number_2);
+            echo"</br>";
+            echo intval($number_3);
+            echo"</br>";
+            echo intval($number_4);
+            echo"</br>";
+                                    
+        ?>
+    </body>
+</html>
