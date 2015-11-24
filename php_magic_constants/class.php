9ad4dd4949a43044f66a8627bba3225d376d7864
shubhobd <shubhomedia@gmail.com>
2015-11-24 19:33:49 +0600

    class file update

---
+++ php_magic_constants/class.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<!--
+    MD.OWES QURUNY SHUBHO
+    http://shubho.info
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+            class basis
+            {
+                public static function bitm(){
+                echo __CLASS__;
+                }
+            }   
+        BASIS::bitm();
+        ?>
+    </body>
+</html>
---
+++ php_magic_constants/method.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<!--
+    MD.OWES QURUNY SHUBHO
+    http://shubho.info
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+            class BASIS
+                    {
+                        public static function BITM(){
+                            echo "this name of method is :" .__METHOD__;
+                        }
+                    }
+                    BASIS::BITM();
+        ?>
+    </body>
+</html>
