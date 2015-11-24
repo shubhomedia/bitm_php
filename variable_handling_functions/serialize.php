2973de237984a7188cd812a2d1eedf92eed968a1
shubhobd <shubhomedia@gmail.com>
2015-11-24 13:58:43 +0600

    serialize file add

---
+++ variable_handling_functions/serialize.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<!--
+    MD.OWES QURUNY SHUBHO
+    http://shubho.info
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+            //serialize — Serialize the value
+            
+            $car = array ("BMW", "VOLVO", "TOYOTA");
+            
+            echo serialize($car);
+            echo"</br>";
+            
+            
+            
+        ?>
+    </body>
+</html>
