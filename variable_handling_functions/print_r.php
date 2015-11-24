6d7fea30951a5bb0b927c888a004612dc258d37a
shubhobd <shubhomedia@gmail.com>
2015-11-24 13:55:00 +0600

    print_r file add

---
+++ variable_handling_functions/print_r.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<!--
+    MD.OWES QURUNY SHUBHO
+    http://shubho.info
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+            //print_r — Prints human-readable information about a variable
+            
+            $blank = NULL;
+            $number = 100;
+            $name = "MD.OWES QURUNY SHUBHO";
+            $car = array ("BMW", "VOLVO", "TOYOTA");
+            
+            print_r($blank);
+            echo"</br>";
+            
+            print_r($number);
+            echo"</br>";
+            
+            print_r($name);
+            echo"</br>";
+            
+            print_r($car);
+            echo"</br>";
+            
+        ?>
+    </body>
+</html>
