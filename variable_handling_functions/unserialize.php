4e94af5412d3f7072beaa4091164e97991fbafaa
shubhobd <shubhomedia@gmail.com>
2015-11-24 14:47:21 +0600

    unserialize file added

---
+++ variable_handling_functions/unserialize.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<!--
+    MD.OWES QURUNY SHUBHO
+    http://shubho.info
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+            //unserialize — unSerialize the value
+            
+            $car = array ("BMW", "VOLVO", "TOYOTA");
+            
+            echo unserialize($car);
+            echo"</br>";
+            
+            
+            
+            
+        ?>
+    </body>
+</html>
