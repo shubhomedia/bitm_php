5f3e0a2e9ffb064289f9001fe32a7df4d7af5ae0
shubhobd <shubhomedia@gmail.com>
2015-11-24 10:57:36 +0600

    float file update

---
+++ variable_handling_functions/floatval.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<!--
+    MD.OWES QURUNY SHUBHO
+    http://shubho.info
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+            //floatval — Get the float value of a variable
+            
+            $var = '122.34343The';
+            $float_value_of_var = floatval($var);
+            echo $float_value_of_var; // 122.34343
+            
+        ?>
+    </body>
+</html>
