9d620ed0a8433f8e06d00a686435ada026a5a3b5
shubhobd <shubhomedia@gmail.com>
2015-11-24 08:41:47 +0600

    array file add

---
+++ data_types/array.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<!--
+    MD.OWES QURUNY SHUBHO
+    http://shubho.info
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+            //PHP Indexed Arrays
+            
+            $car = array ("BMW", "VOLVO", "TOYOTA");
+            var_dump($car);
+            echo"</br>";
+            
+            echo $car[0];
+            echo"</br>";
+            
+            echo $car[1];
+            echo"</br>";
+            
+            echo $car[2];
+            echo"</br>";
+            
+            
+            //PHP Associative Arrays
+            
+            $age = array("Peter"=>"35", "Ben"=>"37", "Joe"=>"43");
+            var_dump($age);
+            echo"</br>";
+            
+            echo $age["Ben"];
+            
+        ?>
+    </body>
+</html>
