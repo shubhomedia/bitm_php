776024537abf71540c7ef5f6d48e7660922b966e
shubhobd <shubhomedia@gmail.com>
2015-11-24 08:32:02 +0600

    float file add

---
+++ data_types/float.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<!--
+    MD.OWES QURUNY SHUBHO
+    http://shubho.info
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+            $float_number = 100.1000;
+            $negetive_number = -100.1000;
+            
+            var_dump($float_number);
+            echo"</br>";
+            echo"$negetive_number";
+        ?>
+    </body>
+</html>
