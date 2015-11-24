a92f31028dd6d9d55bd430ab19bad4454424825f
shubhobd <shubhomedia@gmail.com>
2015-11-24 13:50:56 +0600

    isset file add

---
+++ variable_handling_functions/isset.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<!--
+    MD.OWES QURUNY SHUBHO
+    http://shubho.info
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+            //isset — Determine if a variable is set and is not NULL
+            $blank = NULL;
+            $number = 100;
+            
+            echo isset($blank);
+            echo"</br>";
+            
+            echo isset($number);
+            echo"</br>";
+            
+        ?>
+    </body>
+</html>
