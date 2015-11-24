9c2d61f6e4c1a9f65e5d1bbf028c7450bebea7e5
shubhobd <shubhomedia@gmail.com>
2015-11-24 11:31:15 +0600

    is-array file creat

---
+++ variable_handling_functions/is_array.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<!--
+    MD.OWES QURUNY SHUBHO
+    http://shubho.info
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </he$no = 'this is a string';ad>
+    <body>
+        <?php
+            //is_array — Finds whether a variable is an array
+            
+            $car = array ("BMW", "VOLVO", "TOYOTA");
+            echo is_array($car); //output is boolean 1(true) or 0 or blank (false)
+            echo"</br>";
+            
+            $float_number = 100.1000;
+            echo is_array($float_number); //output is boolean 1(true) or blank (false)
+        ?>
+    </body>
+</html>
