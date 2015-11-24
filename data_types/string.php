7a424fbc5c7886cff77ddf90acf42ce1011a3021
shubhobd <shubhomedia@gmail.com>
2015-11-24 08:25:01 +0600

    string file add

---
+++ data_types/string.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<!--
+    MD.OWES QURUNY SHUBHO
+    http://shubho.info
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+            $string_variable = "Shubho";
+            $name = "MD.OWES QURUNY SHUBHO";
+            $institute = "BASIS Institute of Technology and Management";
+            
+            echo"$string_variable </br>";
+            echo"$name  " . "$institute" . "</br>";
+            echo"Thank You";
+            
+        ?>
+    </body>
+</html>
