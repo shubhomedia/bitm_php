57bda53fac76ee886e5f98afc6cd9d6dd37bab76
shubhobd <shubhomedia@gmail.com>
2015-11-24 08:19:42 +0600

    null file add

---
+++ data_types/null.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<!--
+    MD.OWES QURUNY SHUBHO
+    http://shubho.info
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+            $null_variable = NULL;
+            var_dump($null_variable);
+        ?>
+    </body>
+</html>
