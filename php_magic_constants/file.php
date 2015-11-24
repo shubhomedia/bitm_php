f70d307bd76669353a74b646334de2de78026255
shubhobd <shubhomedia@gmail.com>
2015-11-24 19:19:38 +0600

    file add

---
+++ php_magic_constants/file.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<!--
+    MD.OWES QURUNY SHUBHO
+    http://shubho.info
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+            //__FILE__ PHP magic constant
+           echo"This is FILE: " . __FILE__;
+           
+        ?>
+    </body>
+</html>
