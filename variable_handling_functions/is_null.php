720d8e2a0b05e11ccbf48d302ca90dff08d45fc3
shubhobd <shubhomedia@gmail.com>
2015-11-24 11:38:38 +0600

    is-array file update and is_null file created

--- variable_handling_functions/is_null.php
+++ variable_handling_functions/is_null.php
@@ -7,17 +7,17 @@
     <head>
         <meta charset="UTF-8">
         <title></title>
-    </head>
+    </he$no = 'this is a string';ad>
     <body>
         <?php
             //is_array — Finds whether a variable is an array
-            $no = 'this is a string';
+            
             $car = array ("BMW", "VOLVO", "TOYOTA");
-            echo is_array($car); //output is boolean 1(true) or 0 (false)
-            echo"</br>";
-            echo is_array($no); //output is boolean 1(true) or 0 (false)
+            echo is_array($car); //output is boolean 1(true) or 0 or blank (false)
             echo"</br>";
             
+            $float_number = 100.1000;
+            echo is_array($float_number); //output is boolean 1(true) or blank (false)
         ?>
     </body>
 </html>
