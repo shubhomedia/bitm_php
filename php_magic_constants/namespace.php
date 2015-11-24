e2197a07461c32f41d0ccf4533469399f3d60dc3
shubhobd <shubhomedia@gmail.com>
2015-11-24 19:27:48 +0600

    namespace file add

---
+++ php_magic_constants/namespace.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<!--
+    MD.OWES QURUNY SHUBHO
+    http://shubho.info
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+            $namespace = bitm;
+            echo "the namespace is : " . __NAMESPACE__;
+        ?>
+    </body>
+</html>
