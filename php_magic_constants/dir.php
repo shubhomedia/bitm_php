42d9d670732415a9f7517e5c4fef54182a1b2065
shubhobd <shubhomedia@gmail.com>
2015-11-24 19:18:08 +0600

    dir file add

---
+++ php_magic_constants/dir.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<!--
+    MD.OWES QURUNY SHUBHO
+    http://shubho.info
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+            //__DIR__ PHP magic constant
+           echo"This is DIR: " . __DIR__;
+           echo"</br>";
+           echo"This is DIR: " .__DIR__;
+                       
+        ?>
+    </body>
+</html>
