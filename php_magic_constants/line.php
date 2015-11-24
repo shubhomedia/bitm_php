ea9c898eb3ae0701bb4cf3632b9a6470a731f1e6
shubhobd <shubhomedia@gmail.com>
2015-11-24 19:15:52 +0600

    magic constant folder and line file add

---
+++ php_magic_constants/line.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<!--
+    MD.OWES QURUNY SHUBHO
+    http://shubho.info
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+            //__LINE__ PHP magic constant
+           echo"This is Line: " . __LINE__;
+           echo"</br>";
+           echo"This is Line: " .__LINE__;
+           echo"</br>";
+           echo"This is Line: " .__LINE__;
+           echo"</br>";
+           echo"This is Line: " .__LINE__;            
+        ?>
+    </body>
+</html>
