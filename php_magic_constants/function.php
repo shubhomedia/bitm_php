8775b97a21d82ee964827dd908ccd8f0ed7e2035
shubhobd <shubhomedia@gmail.com>
2015-11-24 19:21:37 +0600

    function file add

---
+++ php_magic_constants/function.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<!--
+    MD.OWES QURUNY SHUBHO
+    http://shubho.info
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+            //__FUNCTION__ PHP magic constant
+            function shubho(){
+                return "My Name is Owes Quruny " .__FUNCTION__;
+            }
+            echo shubho();
+                       
+        ?>
+    </body>
+</html>
