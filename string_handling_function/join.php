21482aab3cd9fb3576564e1a309e61234799f6c7
shubhobd <shubhomedia@gmail.com>
2015-11-28 21:33:19 +0600

    join php fiel added

---
+++ string_handling_function/join.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<!--
+    MD.Owes Quruny Shubho
+    http://shubho.info
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+            $array = array('1', '2', '3', '4', '5', '6', '7', '8', '9', '10');    
+            echo join('<br>', $array);
+            //example (result)
+            /*
+            1
+            2
+            3
+            4
+            5
+            6
+            7
+            8
+            9
+            10
+            */
+        ?>
+    </body>
+</html>
