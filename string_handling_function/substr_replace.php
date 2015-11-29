f8505ff7e754fb1cdbe40675e2a272a7a6daa903
shubhobd <shubhomedia@gmail.com>
2015-11-29 09:08:47 +0600

    substr_replace file add and it works

---
+++ string_handling_function/substr_replace.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<!--
+    MD.Owes Quruny Shubho
+    http://shubho.info
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+            echo substr_replace("Hello","world",0); 
+            // 0 will start replacing at the first character in the string
+        ?>
+    </body>
+</html>
