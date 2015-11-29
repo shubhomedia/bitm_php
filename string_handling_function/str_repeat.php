d972a66d60124591ee8d6e117232dd1216569cb8
shubhobd <shubhomedia@gmail.com>
2015-11-29 08:37:15 +0600

    str_repeat file add and it works

---
+++ string_handling_function/str_repeat.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<!--
+    MD.Owes Quruny Shubho
+    http://shubho.info
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+            echo str_repeat("-=", 80);
+            echo"<br>";
+            echo str_repeat("Shubho ", 20);
+            echo"<br>";
+            echo str_repeat("PHP str_repeat Function ", 2);
+            
+        ?>
+    </body>
+</html>
