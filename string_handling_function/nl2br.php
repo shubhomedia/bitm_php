19cdf80ea63715d6abc9360289cbcec5dab9087a
shubhobd <shubhomedia@gmail.com>
2015-11-29 08:26:19 +0600

    nl2br file add and it works

---
+++ string_handling_function/nl2br.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<!--
+    MD.Owes Quruny Shubho
+    http://shubho.info
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+            echo nl2br("Welcome\r\nThis is my HTML document\n An other line");
+        ?>
+    </body>
+</html>
