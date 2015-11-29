717d5bac0999ca752ff1caadc2b502f81976bbb2
shubhobd <shubhomedia@gmail.com>
2015-11-29 08:58:25 +0600

    strtoupper file add and it works

---
+++ string_handling_function/strtoupper.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<!--
+    MD.Owes Quruny Shubho
+    http://shubho.info
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+            
+            $str = 'ABcdEf';
+            echo strtoupper($str); 
+            
+            echo "<br>";
+            
+            $name = 'MD. OWES QURUNY SHUBHO';
+            echo strtoupper($name); // 22
+            
+            echo "<br>";
+            
+            $str2 = ' ab cd ';
+            echo strtoupper($str2); // 7
+            
+        ?>
+    </body>
+</html>
