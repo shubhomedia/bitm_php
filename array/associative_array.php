85b696eb086eb013917338a336d545199829e1ca
shubhobd <shubhomedia@gmail.com>
2015-11-29 10:22:26 +0600

    array folder creat, indexed_array file and associative_array file creat

---
+++ array/associative_array.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<!--
+    MD.Owes Quruny Shubho
+    http://shubho.info
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+            $age = array(
+                "Peter"=>"35", "Ben"=>"37", "Joe"=>"43","Shubho"=>"23"
+                );
+            echo$age["Peter"];
+            echo"<br>";
+            
+            echo"<pre>";
+            print_r($age);
+            echo"<pre>";
+        ?>
+    </body>
+</html>
---
+++ array/indexed_arrays.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<!--
+    MD.Owes Quruny Shubho
+    http://shubho.info
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+            $cars = array("Volvo", "BMW", "Toyota","Scania","Tata");
+            
+            echo"<pre>";
+            var_dump($cars);
+            echo"<pre>";
+            
+            echo"<br>";
+            echo"<pre>";
+            print_r($cars);
+            echo"<pre>";
+            
+            
+        ?>
+    </body>
+</html>
