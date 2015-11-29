7965b843663f6acfe9f97b920a2ba60a8b06a64f
shubhobd <shubhomedia@gmail.com>
2015-11-29 08:53:08 +0600

    strlen file add and it works

---
+++ string_handling_function/strlen.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<!--
+    MD.Owes Quruny Shubho
+    http://shubho.info
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+            $str = 'abcdef';
+            echo strlen($str); // 6
+            
+            echo "<br>";
+            
+            $name = 'MD. OWES QURUNY SHUBHO';
+            echo strlen($name); // 22
+            
+            echo "<br>";
+            
+            $str2 = ' ab cd ';
+            echo strlen($str); // 7
+        ?>
+    </body>
+</html>
