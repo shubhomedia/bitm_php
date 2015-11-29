453b16bee30b6d93072668ba11487e77b9655eaa
shubhobd <shubhomedia@gmail.com>
2015-11-29 08:45:19 +0600

    str_replace file add and it works

---
+++ string_handling_function/str_replace.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<!--
+    MD.Owes Quruny Shubho
+    http://shubho.info
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+            echo str_replace("world","Peter","Hello world!");
+            //out-put => Hello Peter
+        ?>
+    </body>
+</html>
