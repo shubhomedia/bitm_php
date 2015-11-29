6ea3612e344c2d70b8d8fa9a30ba5601e18ff219
shubhobd <shubhomedia@gmail.com>
2015-11-29 09:14:16 +0600

    ucwords file add and it works

---
+++ string_handling_function/ucwords.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<!--
+    MD.Owes Quruny Shubho
+    http://shubho.info
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+            $name ="md. owes shubho";
+            echo ucwords($name);
+        ?>
+    </body>
+</html>
