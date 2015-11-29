294835ba00e1f7f64a719fd0478e68f46d1bcef5
shubhobd <shubhomedia@gmail.com>
2015-11-29 10:40:59 +0600

    two_dimenstonal_ array file add

---
+++ array/two_dimensional_array.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<!--
+    MD.Owes Quruny Shubho
+    http://shubho.info
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+            
+            $country = array(
+                array(
+                    "Bangladesh","India","Pakistan","Nepal",
+                ),
+                array(
+                    "Iraq","Iran","Afganstan","Dubai",
+                ),
+                array(
+                    "America","Canada","German","Itilly",
+                ),
+            );
+            echo"<pre>";
+            var_dump($country);
+            echo"<pre>";
+            
+            echo"<pre>";
+            print_r($country);
+            echo"<pre>";
+        ?>
+    </body>
+</html>
