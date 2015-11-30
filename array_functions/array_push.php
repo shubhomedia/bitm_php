2e16ed73e032089b54551a7843bd90168ddc8ac1
shubhobd <shubhomedia@gmail.com>
2015-11-30 22:53:03 +0600

    array_push file added

---
+++ array_functions/array_push.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<!--
+    MD.Owes Quruny Shubho
+    http://shubho.info
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+            $stack = array("orange", "banana");
+            array_push($stack, "apple", "raspberry");
+            echo"<pre>";
+            print_r($stack);
+            echo"<pre>";
+        ?>
+    </body>
+</html>
