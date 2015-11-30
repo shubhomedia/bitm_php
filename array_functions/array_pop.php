c7bfe9087c15f0347267d91b18ecc8742f3b2c2a
shubhobd <shubhomedia@gmail.com>
2015-11-30 22:50:48 +0600

    array_pop file added

---
+++ array_functions/array_pop.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<!--
+    MD.Owes Quruny Shubho
+    http://shubho.info
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+            $stack = array("orange", "banana", "apple", "raspberry");
+            $fruit = array_pop($stack);
+            echo"<pre>";
+            print_r($stack);
+            echo"<pre>";
+        ?>
+    </body>
+</html>
