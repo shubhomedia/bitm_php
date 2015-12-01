0bc9fad09c468bf393ba32a35f59e035fc513b64
shubhobd <shubhomedia@gmail.com>
2015-12-01 14:46:44 +0600

    loops folser and for_file added

---
+++ loops/for.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<!--
+    MD.Owes Quruny Shubho
+    http://shubho.info
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+            /*
+            for (init counter; test counter; increment counter) {
+                    code to be executed;
+                }
+             */
+        
+             for ($x = 0; $x <= 20; $x++) {
+                echo "The number is: $x <br>";
+                } 
+        ?>
+    </body>
+</html>
