e64e30d9f768e0689dc4283a3d52c337c829ba7e
shubhobd <shubhomedia@gmail.com>
2015-12-01 11:35:53 +0600

    sort file added

---
+++ array_functions/sort.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<!--
+    MD.Owes Quruny Shubho
+    http://shubho.info
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+            $numbers=array(4,6,2,22,11);
+            echo sort($numbers);
+        ?>
+    </body>
+</html>
