118433579a657d2810e7b2c4d1bed61e47d3f4a6
shubhobd <shubhomedia@gmail.com>
2015-12-01 10:56:58 +0600

    count file added

---
+++ array_functions/count.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<!--
+    MD.Owes Quruny Shubho
+    http://shubho.info
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+            $cars=array("Volvo","BMW","Toyota");
+            echo"<pre>";
+            echo count($cars); //3
+            echo"<pre>";
+            
+            echo"<br>";
+            $a = array('Bangladesh', 'India', 'Dubai','India','India', );
+            echo count($a); //5 
+        ?>
+        
+    </body>
+</html>
