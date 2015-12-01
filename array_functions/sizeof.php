234029fd861b1e275b5d63d7cacf8da56d172fea
shubhobd <shubhomedia@gmail.com>
2015-12-01 11:33:10 +0600

    size of file added and it's similar to count

---
+++ array_functions/sizeof.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<!--
+    MD.Owes Quruny Shubho
+    http://shubho.info
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+            $cars=array("Volvo","BMW","Toyota");
+            echo"<pre>";
+            print_r($cars);
+            echo"<pre>";
+            
+            echo"Total: ". sizeof($cars);
+        ?>
+    </body>
+</html>
