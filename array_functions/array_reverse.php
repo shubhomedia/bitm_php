9cbc53eeffd1ec50d01431113b3add4cbca0f882
shubhobd <shubhomedia@gmail.com>
2015-12-01 08:20:08 +0600

    array_reverse file added

---
+++ array_functions/array_reverse.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<!--
+    MD.Owes Quruny Shubho
+    http://shubho.info
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+            $a=array("a"=>"Volvo","b"=>"BMW","c"=>"Toyota");
+            echo"<pre>";
+            print_r($a);
+            echo"<pre>";
+            
+            echo"<pre>";
+            print_r(array_reverse($a));
+            echo"<pre>";
+        ?>
+    </body>
+</html>
