17756b49560e051212643288f9e1c6b85e8088d8
shubhobd <shubhomedia@gmail.com>
2015-12-01 08:38:22 +0600

    asort file added

---
+++ array_functions/asort.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<!--
+    MD.Owes Quruny Shubho
+    http://shubho.info
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+        
+            $age=array("Peter"=>"35","Ben"=>"37","Joe"=>"43");
+            echo "<pre>";
+            print_r($age);
+            echo "<pre>";
+            
+            echo asort($age);
+                                      
+        ?>
+    </body>
+</html>
