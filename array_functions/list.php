7f2d8d703334254343fafd1c09f98b33f0b275cc
shubhobd <shubhomedia@gmail.com>
2015-12-01 11:22:30 +0600

    list and pos files added

---
+++ array_functions/list.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<!--
+    MD.Owes Quruny Shubho
+    http://shubho.info
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+            $my_array = array("Dog","Cat","Horse");
+
+            list($a, $b, $c) = $my_array;
+            echo "I have several animals, a $a, a $b and a $c.";
+        ?>
+    </body>
+</html>
---
+++ array_functions/pos.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<!--
+    MD.Owes Quruny Shubho
+    http://shubho.info
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+            $people = array("Shubho", "Rony", "Jony", "Anwar");
+            
+            echo" End Value in Array Is: ";
+            echo pos($people) . "<br>";
+            
+        ?>
+    </body>
+</html>
