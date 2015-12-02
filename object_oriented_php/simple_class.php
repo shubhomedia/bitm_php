53f160542c46e04f6c30457b15fcb9a1a7e4a93e
shubhobd <shubhomedia@gmail.com>
2015-12-02 23:23:24 +0600

    OOP In PHP and class file created

---
+++ object_oriented_php/simple_class.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<!--
+    MD.Owes Quruny Shubho
+    http://shubho.info
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+            class MyClass
+                {
+                  // Class properties and methods go here
+                }
+                $obj = new MyClass;
+                
+                
+                var_dump($obj);
+        ?>
+    </body>
+</html>
