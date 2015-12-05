ea15ae55feddadf069510aa818c27ec868695060
shubhobd <shubhomedia@gmail.com>
2015-12-05 21:19:48 +0600

    class_2 file added

---
+++ object_oriented_php/class_2.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<!--
+    MD.Owes Quruny Shubho
+    http://shubho.info
+-->
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+            class friends{
+                public $var="Default Value";
+            }
+        ?>
+    </body>
+</html>
